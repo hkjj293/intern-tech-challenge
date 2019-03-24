724f88a32f2dcdc2030f559a4dfae3b8b5a28480
hkjj293 <chrishuihuihui@gmail.com>
2019-03-24 15:46:32 +0800

    Change in error handling

--- main.go
+++ main.go
@@ -16,33 +16,47 @@ func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*s
 	return versionSlice
 }
 
+func gitError(err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("%s", r)
+		}
+	}()
+	panic(err)
+}
+
 // Here we implement the basics of communicating with github through the library as well as printing the version
 // You will need to implement LatestVersions function as well as make this application support the file format outlined in the README
 // Please use the format defined by the fmt.Printf line at the bottom, as we will define a passing coding challenge as one that outputs
 // the correct information, including this line
 func main() {
+	// Get input arg which is file path name
+
 	// Github
 	client := github.NewClient(nil)
 	ctx := context.Background()
 	opt := &github.ListOptions{PerPage: 10}
-	releases, _, err := client.Repositories.ListReleases(ctx, "kubernetes", "kubernetes", opt)
+	//releases, _, err := client.Repositories.ListReleases(ctx, "kubernetes", "kubernetes", opt)
+	releases, _, err := client.Repositories.ListReleases(ctx, "lkubernetes", "kulbernetes", opt)
 	if err != nil {
-		panic(err) // is this really a good way?
+		// Try to recover and continue to run
+		gitError(err)
+		//panic(err) // is this really a good way?
+		/*No if we still want  to  retrieve  the relaese*/
+		/*version of other open source software			*/
 	}
+	// Debugging printout
+	//fmt.Printf("Hello")
 	minVersion := semver.New("1.8.0")
 	allReleases := make([]*semver.Version, len(releases))
 	for i, release := range releases {
-		// Debugging printout
-		//fmt.Printf("%s ", *release.TagName)
 		versionString := *release.TagName
 		if versionString[0] == 'v' {
 			versionString = versionString[1:]
-			// Debugging printout
-			//fmt.Printf("%s ", versionString)
 		}
 		allReleases[i] = semver.New(versionString)
 	}
 	versionSlice := LatestVersions(allReleases, minVersion)
-	
+
 	fmt.Printf("latest versions of kubernetes/kubernetes: %s", versionSlice)
 }
